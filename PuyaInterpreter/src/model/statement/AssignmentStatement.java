04455e06ee3a9640de4111731760a8036099d6b9
GhermanCristian <gherman_cristi10@yahoo.com>
2020-10-18 17:26:45 +0300

    Lab 3 - Added the AssignmentStatement

---
+++ PuyaInterpreter/src/model/statement/AssignmentStatement.java
@@ -0,0 +1,48 @@
+package model.statement;
+
+import model.ProgramState;
+import model.ADT.DictionaryInterface;
+import model.expression.ExpressionInterface;
+import model.type.TypeInterface;
+import model.value.ValueInterface;
+
+public class AssignmentStatement implements StatementInterface{
+	private final String variableName; // left operator
+	private final ExpressionInterface expression;  // right operator
+
+	public AssignmentStatement(String variableName, ExpressionInterface expression) {
+		this.variableName = variableName;
+		this.expression = expression;
+	}
+	
+	@Override
+	public ProgramState execute(ProgramState crtState) throws Exception {
+		DictionaryInterface<String, ValueInterface> symbolTable = crtState.getSymbolTable();
+		
+		if (symbolTable.isDefined(this.variableName) == false) {
+			throw new Exception("Variable " + this.variableName + " is not defined");
+		}
+		
+		// the value of the new expression (the one we want to assign to the existing variable)
+		ValueInterface newExpressionValue = this.expression.evaluate(symbolTable);
+		TypeInterface newExpressionType = newExpressionValue.getType();
+		TypeInterface variableType = symbolTable.getValue(this.variableName).getType();
+		
+		// if the new expression's type matches the type of the existing variable => update
+		if (variableType.equals(newExpressionType)) {
+			symbolTable.update(this.variableName, newExpressionValue);
+		}
+		else {
+			throw new Exception("Type of " + this.variableName + " doesn't match type of expression");
+		}
+		
+		return crtState;
+	}
+	
+	public String toString() {
+		String representation = "";
+		representation += (this.variableName + " = " + this.expression.toString());
+		return representation;
+	}
+
+}
