2fd5d5c1e4895f7378c04e9a318d7d574cf43858
GhermanCristian <gherman_cristi10@yahoo.com>
2020-10-26 15:18:02 +0200

    Lab 4 - Added the other two program examples in View

--- PuyaInterpreter/src/view/View.java
+++ PuyaInterpreter/src/view/View.java
@@ -10,14 +10,18 @@ import model.ADT.MyDictionary;
 import model.ADT.MyList;
 import model.ADT.MyStack;
 import model.ADT.StackInterface;
+import model.expression.ArithmeticExpression;
 import model.expression.ValueExpression;
 import model.expression.VariableExpression;
 import model.statement.AssignmentStatement;
 import model.statement.CompoundStatement;
+import model.statement.IfStatement;
 import model.statement.PrintStatement;
 import model.statement.StatementInterface;
 import model.statement.VariableDeclarationStatement;
+import model.type.BoolType;
 import model.type.IntType;
+import model.value.BoolValue;
 import model.value.IntValue;
 import model.value.ValueInterface;
 
@@ -45,15 +49,58 @@ public class View {
 			}
 			
 			if (choice == 1) {
-				// int a; a = 23; print(a);
+				/*// int a; a = 23; print(a);
 				StatementInterface originalProgram = new CompoundStatement(
 					new CompoundStatement(
 						new VariableDeclarationStatement("a", new IntType()), 
 						new AssignmentStatement("a", new ValueExpression(new IntValue(5)))
 					),
 					new PrintStatement(new VariableExpression("a"))
-				);
+				);*/
 				
+				// int a; int b; a = 2 + 3 * 5; b = a + 1; print(b);
+				/*StatementInterface originalProgram = new CompoundStatement(
+					new VariableDeclarationStatement("a", new IntType()), 
+					new CompoundStatement(
+						new VariableDeclarationStatement("b", new IntType()), 
+						new CompoundStatement(
+							new AssignmentStatement("a", new ArithmeticExpression(
+									new ArithmeticExpression(
+											new ValueExpression(new IntValue(3)), 
+											new ValueExpression(new IntValue(5)), 
+											2), // 3 * 5 
+									new ValueExpression(new IntValue(2)), 
+									0)), // 3 * 5 + 2 
+							new CompoundStatement(
+								new AssignmentStatement("b", new ArithmeticExpression(
+									new VariableExpression("a"), 
+									new ValueExpression(new IntValue(1)), 
+									0)), // b = a + 1
+								new PrintStatement(new VariableExpression("b")) // print(b)
+							)
+						)
+					)
+				);*/
+				
+				//bool a; int v; a=true;(If a Then v=2 Else v=3);Print(v)
+				StatementInterface originalProgram = new CompoundStatement(
+					new VariableDeclarationStatement("a", new BoolType()), 
+					new CompoundStatement(
+						new VariableDeclarationStatement("v", new IntType()), 
+						new CompoundStatement(
+							new AssignmentStatement("a", new ValueExpression(new BoolValue(true))),
+							new CompoundStatement(
+								new IfStatement(
+									new VariableExpression("a"), 
+									new AssignmentStatement("v", new ValueExpression(new IntValue(2))), 
+									new AssignmentStatement("v", new ValueExpression(new IntValue(3)))
+								),
+								new PrintStatement(new VariableExpression("v"))
+							)
+						)
+					)
+				);
+						
 				StackInterface<StatementInterface> stack = new MyStack<StatementInterface>();
 				DictionaryInterface<String, ValueInterface> symbolTable = new MyDictionary<String, ValueInterface>();
 				ListInterface<ValueInterface> output = new MyList<ValueInterface>();
