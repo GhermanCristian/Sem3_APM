d2eebcd1d9863ca4bfa5c39621051934552595bc
GhermanCristian <gherman_cristi10@yahoo.com>
2020-10-30 12:15:21 +0200

    Lab 5 - Added a method that transforms a list of statements into a compound statement - Separated the examples from View into different methods

--- PuyaInterpreter/src/view/View.java
+++ PuyaInterpreter/src/view/View.java
@@ -15,6 +15,7 @@ import model.expression.ValueExpression;
 import model.expression.VariableExpression;
 import model.statement.AssignmentStatement;
 import model.statement.CompoundStatement;
+import model.statement.EmptyStatement;
 import model.statement.IfStatement;
 import model.statement.PrintStatement;
 import model.statement.StatementInterface;
@@ -30,6 +31,71 @@ public class View {
 	
 	public View() {
 		this.controller = new Controller();
+	}
+	
+	private StatementInterface composeStatement(MyList<StatementInterface> crtList) throws Exception {
+		// pop never actually throws an exception here (I use it when size() >= 1, and the exception occurs when size() = 0)
+		// but if I didn't add it, the compiler would complain
+		if (crtList.size() == 0) {
+			return new EmptyStatement();
+		}
+		
+		if (crtList.size() == 1) {
+			return crtList.pop();
+		}
+		
+		StatementInterface lastStatement = crtList.pop();
+		return new CompoundStatement(composeStatement(crtList), lastStatement);
+	}
+	
+	private MyList<StatementInterface> getFirstExample() {
+		MyList<StatementInterface> statementList = new MyList<StatementInterface>();
+		
+		// int a; a = 23; print(a);
+		statementList.addLast(new VariableDeclarationStatement("a", new IntType()));
+		statementList.addLast(new AssignmentStatement("a", new ValueExpression(new IntValue(23))));
+		statementList.addLast(new PrintStatement(new VariableExpression("a")));
+		
+		return statementList;
+	}
+	
+	private MyList<StatementInterface> getSecondExample() {
+		MyList<StatementInterface> statementList = new MyList<StatementInterface>();
+		
+		// int a; int b; a = 2 + 3 * 5; b = a + 1; print(b);
+		statementList.addLast(new VariableDeclarationStatement("a", new IntType()));
+		statementList.addLast(new VariableDeclarationStatement("b", new IntType()));
+		statementList.addLast(new AssignmentStatement("a", new ArithmeticExpression(
+								new ArithmeticExpression(
+										new ValueExpression(new IntValue(3)), 
+										new ValueExpression(new IntValue(5)), 
+										2), // 3 * 5 
+								new ValueExpression(new IntValue(2)), 
+								0))); // 3 * 5 + 2 );
+		statementList.addLast(new AssignmentStatement("b", new ArithmeticExpression(
+								new VariableExpression("a"), 
+								new ValueExpression(new IntValue(1)), 
+								0))); // b = a + 1);
+		statementList.addLast(new PrintStatement(new VariableExpression("b")));
+		
+		return statementList;
+	}
+	
+	private MyList<StatementInterface> getThirdExample() {
+		MyList<StatementInterface> statementList = new MyList<StatementInterface>();
+		
+		//bool a; int v; a=true; (If a Then v=2 Else v=3); Print(v)
+		statementList.addLast(new VariableDeclarationStatement("a", new BoolType()));
+		statementList.addLast(new VariableDeclarationStatement("v", new IntType()));
+		statementList.addLast(new AssignmentStatement("a", new ValueExpression(new BoolValue(true))));
+		statementList.addLast(new IfStatement(
+								new VariableExpression("a"), 
+								new AssignmentStatement("v", new ValueExpression(new IntValue(2))), 
+								new AssignmentStatement("v", new ValueExpression(new IntValue(3)))
+							));
+		statementList.addLast(new PrintStatement(new VariableExpression("v")));
+		
+		return statementList;
 	}
 	
 	public void start() {
@@ -49,57 +115,23 @@ public class View {
 			}
 			
 			if (choice == 1) {
-				/*// int a; a = 23; print(a);
-				StatementInterface originalProgram = new CompoundStatement(
-					new CompoundStatement(
-						new VariableDeclarationStatement("a", new IntType()), 
-						new AssignmentStatement("a", new ValueExpression(new IntValue(5)))
-					),
-					new PrintStatement(new VariableExpression("a"))
-				);*/
+				MyList<StatementInterface> statementList = new MyList<StatementInterface>();
 				
+				// int a; a = 23; print(a);
+				statementList = this.getFirstExample();
 				// int a; int b; a = 2 + 3 * 5; b = a + 1; print(b);
-				/*StatementInterface originalProgram = new CompoundStatement(
-					new VariableDeclarationStatement("a", new IntType()), 
-					new CompoundStatement(
-						new VariableDeclarationStatement("b", new IntType()), 
-						new CompoundStatement(
-							new AssignmentStatement("a", new ArithmeticExpression(
-									new ArithmeticExpression(
-											new ValueExpression(new IntValue(3)), 
-											new ValueExpression(new IntValue(5)), 
-											2), // 3 * 5 
-									new ValueExpression(new IntValue(2)), 
-									0)), // 3 * 5 + 2 
-							new CompoundStatement(
-								new AssignmentStatement("b", new ArithmeticExpression(
-									new VariableExpression("a"), 
-									new ValueExpression(new IntValue(1)), 
-									0)), // b = a + 1
-								new PrintStatement(new VariableExpression("b")) // print(b)
-							)
-						)
-					)
-				);*/
+				//statementList = this.getSecondExample();
+				//bool a; int v; a=true; (If a Then v=2 Else v=3); Print(v)
+				//statementList = this.getThirdExample();
 				
-				//bool a; int v; a=true;(If a Then v=2 Else v=3);Print(v)
-				StatementInterface originalProgram = new CompoundStatement(
-					new VariableDeclarationStatement("a", new BoolType()), 
-					new CompoundStatement(
-						new VariableDeclarationStatement("v", new IntType()), 
-						new CompoundStatement(
-							new AssignmentStatement("a", new ValueExpression(new BoolValue(true))),
-							new CompoundStatement(
-								new IfStatement(
-									new VariableExpression("a"), 
-									new AssignmentStatement("v", new ValueExpression(new IntValue(2))), 
-									new AssignmentStatement("v", new ValueExpression(new IntValue(3)))
-								),
-								new PrintStatement(new VariableExpression("v"))
-							)
-						)
-					)
-				);
+				StatementInterface originalProgram;
+				try {
+					originalProgram = this.composeStatement(statementList);
+				}
+				catch (Exception e) {
+					System.out.println(e.getMessage());
+					continue;
+				}
 						
 				StackInterface<StatementInterface> stack = new MyStack<StatementInterface>();
 				DictionaryInterface<String, ValueInterface> symbolTable = new MyDictionary<String, ValueInterface>();
