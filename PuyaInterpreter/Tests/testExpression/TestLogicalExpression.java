758a4cb8921b1200afca691cd6f5825312d9c9df
GhermanCristian <gherman_cristi10@yahoo.com>
2020-11-08 11:11:21 +0200

    Lab 6 - Added TestLogicalExpression

---
+++ PuyaInterpreter/Tests/testExpression/TestLogicalExpression.java
@@ -0,0 +1,139 @@
+package testExpression;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import org.junit.*;
+import model.ProgramState;
+import model.ADT.DictionaryInterface;
+import model.ADT.ListInterface;
+import model.ADT.MyDictionary;
+import model.ADT.MyList;
+import model.ADT.MyStack;
+import model.ADT.StackInterface;
+import model.expression.ExpressionInterface;
+import model.expression.LogicalExpression;
+import model.expression.ValueExpression;
+import model.statement.StatementInterface;
+import model.value.BoolValue;
+import model.value.IntValue;
+import model.value.StringValue;
+import model.value.ValueInterface;
+
+public class TestLogicalExpression {
+	static StackInterface<StatementInterface> stack;
+	static DictionaryInterface<String, ValueInterface> symbolTable;
+	static ListInterface<ValueInterface> output;
+	static DictionaryInterface<StringValue, BufferedReader> fileTable;
+	static ProgramState crtState;
+	
+	@BeforeClass
+	public static void initialiseData() {
+		stack = new MyStack<StatementInterface>();
+		symbolTable = new MyDictionary<String, ValueInterface>();
+		output = new MyList<ValueInterface>();
+		fileTable = new MyDictionary<StringValue, BufferedReader>();
+		crtState = new ProgramState(stack, symbolTable, output, fileTable, null);
+	}
+	
+	@After
+	public void clearAndCloseData() {
+		stack.clear();
+		symbolTable.clear();
+		output.clear();
+		
+		for (BufferedReader crtBuffer : fileTable.getAllValues()) {
+			try {
+				crtBuffer.close();
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		fileTable.clear();
+	}
+	
+	@Test
+	public void Evaluate_InvalidOperator_ThrowsException() {
+		ExpressionInterface e1 = new LogicalExpression(
+			new ValueExpression(new BoolValue(true)), 
+			new ValueExpression(new BoolValue(true)), 
+			"invalidOperator");
+		
+		try {
+			e1.evaluate(symbolTable);
+			fail("Invalid operator");
+		}
+		catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void Evaluate_FirstOperandNotBoolean_ThrowsException() {
+		ExpressionInterface e1 = new LogicalExpression(
+			new ValueExpression(new IntValue(12)), 
+			new ValueExpression(new BoolValue(true)), 
+			"||");
+		
+		try {
+			e1.evaluate(symbolTable);
+			fail("First operand should be boolean");
+		}
+		catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void Evaluate_SecondOperandNotBoolean_ThrowsException() {
+		ExpressionInterface e1 = new LogicalExpression(
+			new ValueExpression(new BoolValue(true)), 
+			new ValueExpression(new IntValue(12)), 
+			"||");
+			
+		try {
+			e1.evaluate(symbolTable);
+			fail("Second operand should be boolean");
+		}
+		catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void Evaluate_ValidAND_CorrectResult() {
+		ExpressionInterface e1 = new LogicalExpression(
+			new ValueExpression(new BoolValue(true)), 
+			new ValueExpression(new BoolValue(false)), 
+			"&&");
+		
+		ValueInterface res = null;
+		try {
+			res = e1.evaluate(symbolTable);
+		} 
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertFalse(((BoolValue)res).getValue());
+	}
+	
+	@Test
+	public void Evaluate_ValidOR_CorrectResult() {
+		ExpressionInterface e1 = new LogicalExpression(
+			new ValueExpression(new BoolValue(true)), 
+			new ValueExpression(new BoolValue(false)), 
+			"||");
+			
+		ValueInterface res = null;
+		try {
+			res = e1.evaluate(symbolTable);
+		} 
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue(((BoolValue)res).getValue());
+	}
+}
