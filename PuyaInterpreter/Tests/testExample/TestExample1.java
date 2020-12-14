18d207f66d7e4f3032255b8117ff5c12e0714dac
GhermanCristian <gherman_cristi10@yahoo.com>
2020-12-14 15:39:51 +0200

    Lab 10 - Added TestExample1 (tests the content of the structures after the execution of example1)

---
+++ PuyaInterpreter/Tests/testExample/TestExample1.java
@@ -0,0 +1,157 @@
+package testExample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+import org.junit.*;
+
+import controller.Controller;
+import controller.TextController;
+import model.Example;
+import model.ProgramState;
+import model.ADT.DictionaryInterface;
+import model.ADT.ListInterface;
+import model.ADT.MyDictionary;
+import model.ADT.MyHeap;
+import model.ADT.MyList;
+import model.ADT.MyStack;
+import model.ADT.StackInterface;
+import model.statement.StatementInterface;
+import model.type.TypeInterface;
+import model.value.IntValue;
+import model.value.StringValue;
+import model.value.ValueInterface;
+import repository.Repository;
+import repository.RepositoryInterface;
+import view.AllExamples;
+
+public class TestExample1 {
+	static StackInterface<StatementInterface> stack;
+	static DictionaryInterface<String, ValueInterface> symbolTable;
+	static ListInterface<ValueInterface> output;
+	static DictionaryInterface<StringValue, BufferedReader> fileTable;
+	static DictionaryInterface<Integer, ValueInterface> heap;
+	static DictionaryInterface<String, TypeInterface> typeEnvironment;
+	static Example example;
+	static ProgramState crtState;
+	static RepositoryInterface repo;
+	static Controller controller;
+	
+	private static final String REPOSITORY_PATH = "C:\\Users\\gherm\\Documents\\EclipseWorkspace\\APM\\PuyaInterpreter\\log1.in";
+	
+	@BeforeClass
+	public static void initialiseData() {
+		stack = new MyStack<StatementInterface>();
+		symbolTable = new MyDictionary<String, ValueInterface>();
+		output = new MyList<ValueInterface>();
+		fileTable = new MyDictionary<StringValue, BufferedReader>();
+		heap = new MyHeap<Integer, ValueInterface>();
+		typeEnvironment = new MyDictionary<String, TypeInterface>();
+		AllExamples allExamples = new AllExamples();
+		example = allExamples.getExample1();
+		crtState = new ProgramState(stack, symbolTable, output, fileTable, heap, example.getStatement());
+		repo = new Repository(REPOSITORY_PATH);
+		controller = new TextController(repo);
+		controller.addProgramState(crtState);
+	}
+	
+	@After
+	public void clearAndCloseData() {
+		stack.clear();
+		symbolTable.clear();
+		output.clear();
+		
+		for (BufferedReader crtBuffer : fileTable.getAllValues()) {
+			try {
+				crtBuffer.close();
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		fileTable.clear();
+		heap.clear();
+		typeEnvironment.clear();
+		
+		try {
+			PrintWriter currentFileWriter = new PrintWriter(REPOSITORY_PATH);
+			currentFileWriter.close(); // I do this to erase the content of the file
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		crtState.setStatement(example.getStatement());
+	}
+	//"int a; a = 23; print(a);"
+	
+	@Test
+	public void FullProgramExecution_Example1_CorrectOutput() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertEquals(output.size(), 1);
+		try {
+			assertEquals(output.get(0), new IntValue(23));
+		} 
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void FullProgramExecution_Example1_CorrectHeapTable() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue(heap.isEmpty());
+	}
+	
+	@Test
+	public void FullProgramExecution_Example1_CorrectFileTable() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue(fileTable.isEmpty());
+	}
+	
+	@Test
+	public void FullProgramExecution_Example1Thread1_EmptyStack() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertEquals(0, stack.size());
+	}
+	
+	@Test
+	public void FullProgramExecution_Example1Thread1_CorrectSymbolTable() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertEquals(1, symbolTable.size());
+		assertEquals(new IntValue(23), symbolTable.getValue("a"));
+	}
+
+}
