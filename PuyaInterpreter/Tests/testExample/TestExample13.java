0d1342a33ee805c0093346bd22d612557286912f
GhermanCristian <gherman_cristi10@yahoo.com>
2020-12-22 17:38:25 +0200

    Lab 11 - Added an example with triple forking (no heap modifications in the inner forks) - Added a TestExample for it

---
+++ PuyaInterpreter/Tests/testExample/TestExample13.java
@@ -0,0 +1,179 @@
+package testExample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import org.junit.*;
+import controller.Controller;
+import controller.TextController;
+import model.Example;
+import model.ProgramState;
+import model.ADT.DictionaryInterface;
+import model.ADT.ListInterface;
+import model.ADT.MyDictionary;
+import model.ADT.MyHeap;
+import model.ADT.MyList;
+import model.ADT.MyStack;
+import model.ADT.StackInterface;
+import model.statement.StatementInterface;
+import model.type.IntType;
+import model.type.TypeInterface;
+import model.value.IntValue;
+import model.value.ReferenceValue;
+import model.value.StringValue;
+import model.value.ValueInterface;
+import repository.Repository;
+import repository.RepositoryInterface;
+import view.AllExamples;
+
+public class TestExample13 {
+	static StackInterface<StatementInterface> stack;
+	static DictionaryInterface<String, ValueInterface> symbolTable;
+	static ListInterface<ValueInterface> output;
+	static DictionaryInterface<StringValue, BufferedReader> fileTable;
+	static DictionaryInterface<Integer, ValueInterface> heap;
+	static DictionaryInterface<String, TypeInterface> typeEnvironment;
+	static Example example;
+	static ProgramState crtState;
+	static RepositoryInterface repo;
+	static Controller controller;
+	
+	private static final String REPOSITORY_PATH = "C:\\Users\\gherm\\Documents\\EclipseWorkspace\\APM\\PuyaInterpreter\\log13.in";
+	
+	@BeforeClass
+	public static void initialiseData() {
+		stack = new MyStack<StatementInterface>();
+		symbolTable = new MyDictionary<String, ValueInterface>();
+		output = new MyList<ValueInterface>();
+		fileTable = new MyDictionary<StringValue, BufferedReader>();
+		heap = new MyHeap<Integer, ValueInterface>();
+		typeEnvironment = new MyDictionary<String, TypeInterface>();
+		AllExamples allExamples = new AllExamples();
+		example = allExamples.getExample13();
+		
+		crtState = new ProgramState(stack, symbolTable, output, fileTable, heap, example.getStatement());
+		repo = new Repository(REPOSITORY_PATH);
+		controller = new TextController(repo);
+		controller.addProgramState(crtState);
+	}
+	
+	@After
+	public void clearAndCloseData() {
+		stack.clear();
+		symbolTable.clear();
+		output.clear();
+		
+		for (BufferedReader crtBuffer : fileTable.getAllValues()) {
+			try {
+				crtBuffer.close();
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		fileTable.clear();
+		heap.clear();
+		typeEnvironment.clear();
+		
+		try {
+			PrintWriter currentFileWriter = new PrintWriter(REPOSITORY_PATH);
+			currentFileWriter.close(); // I do this to erase the content of the file
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		repo.getThreadList().clear();
+		crtState.setStatement(example.getStatement());
+		controller.addProgramState(crtState);
+	}
+	//"int v; Ref int a; v=10; new(a,22); fork(wH(a,30); fork(v=33; fork(v=34; print(v);) print(v); ); print(rH(a)); v=32; print(rH(a))); print(v); print(rH(a));"
+	
+	@Test
+	public void FullProgramExecution_Example13_CorrectOutput() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertEquals(output.size(), 6);
+		try {
+			assertEquals(output.get(0), new IntValue(10));
+			assertEquals(output.get(1), new IntValue(30));
+			assertEquals(output.get(2), new IntValue(30));
+			assertEquals(output.get(3), new IntValue(30));
+			assertEquals(output.get(4), new IntValue(33));
+			assertEquals(output.get(5), new IntValue(34));
+		} 
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void FullProgramExecution_Example13_CorrectHeapTable() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertEquals(heap.size(), 1);
+		assertEquals(heap.getValue(1), new IntValue(30));
+	}
+	
+	@Test
+	public void FullProgramExecution_Example13_CorrectFileTable() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue(fileTable.isEmpty());
+	}
+	
+	// when I have multiple threads, the threadList becomes empty after the execution, so there's no way of checking each tread
+	// crtState will always represent thread1, so we can at least check that
+	@Test
+	public void FullProgramExecution_Example13Thread1_EmptyStack() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertEquals(0, crtState.getExecutionStack().size());
+	}
+	
+	@Test
+	public void FullProgramExecution_Example13Thread1_CorrectSymbolTable() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		assertEquals(2, symbolTable.size());
+		assertEquals(symbolTable.getValue("v"), new IntValue(10));
+		assertEquals(symbolTable.getValue("a"), new ReferenceValue(1, new IntType()));
+	}
+	
+	@Test
+	public void FullProgramExecution_Example13_EmptyThreadList() {
+		try {
+			controller.fullProgramExecution();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue(repo.getThreadList().isEmpty());
+	}
+}
--- PuyaInterpreter/src/view/AllExamples.java
+++ PuyaInterpreter/src/view/AllExamples.java
@@ -283,6 +283,39 @@ public class AllExamples {
 		return new Example(this.composeStatement(statementList), "int v; Ref int a; v=10; new(a,22); fork(wH(a,30); fork(v=33; wH(a,24);); print(rH(a)); v=32; print(rH(a))); print(v); print(rH(a));", this.SRC_FOLDER_PATH + "\\log12.in");
 	}
 	
+	public Example getExample13() {
+		MyList<StatementInterface> statementList = new MyList<StatementInterface>();
+		
+		//int v; Ref int a; v=10; new(a,22); fork(wH(a,30); fork(v=33; fork(v=34; print(v);) print(v); ); print(rH(a)); v=32; print(rH(a))); print(v); print(rH(a));
+		statementList.addLast(new VariableDeclarationStatement("v", new IntType()));
+		statementList.addLast(new VariableDeclarationStatement("a", new ReferenceType(new IntType())));
+		statementList.addLast(new AssignmentStatement("v", new ValueExpression(new IntValue(10))));
+		statementList.addLast(new HeapAllocationStatement("a", new ValueExpression(new IntValue(22))));
+		
+		MyList<StatementInterface> threadStatementList = new MyList<StatementInterface>();
+		threadStatementList.addLast(new HeapWritingStatement("a", new ValueExpression(new IntValue(30))));
+		
+		MyList<StatementInterface> innerThreadStatementList = new MyList<StatementInterface>();
+		innerThreadStatementList.addLast(new AssignmentStatement("v", new ValueExpression(new IntValue(33))));
+		
+		MyList<StatementInterface> innerInnerThreadStatementList = new MyList<StatementInterface>();
+		innerInnerThreadStatementList.addLast(new AssignmentStatement("v", new ValueExpression(new IntValue(34))));
+		innerInnerThreadStatementList.addLast(new PrintStatement(new VariableExpression("v")));
+		innerThreadStatementList.addLast(new ForkStatement(this.composeStatement(innerInnerThreadStatementList)));
+		innerThreadStatementList.addLast(new PrintStatement(new VariableExpression("v")));
+		
+		threadStatementList.addLast(new ForkStatement(this.composeStatement(innerThreadStatementList)));
+		threadStatementList.addLast(new PrintStatement(new HeapReadingExpression(new VariableExpression("a"))));
+		threadStatementList.addLast(new AssignmentStatement("v", new ValueExpression(new IntValue(32))));
+		threadStatementList.addLast(new PrintStatement(new HeapReadingExpression(new VariableExpression("a"))));
+		
+		statementList.addLast(new ForkStatement(this.composeStatement(threadStatementList)));
+		statementList.addLast(new PrintStatement(new VariableExpression("v")));
+		statementList.addLast(new PrintStatement(new HeapReadingExpression(new VariableExpression("a"))));
+		
+		return new Example(this.composeStatement(statementList), "int v; Ref int a; v=10; new(a,22); fork(wH(a,30); fork(v=33; fork(v=34; print(v);) print(v); ); print(rH(a)); v=32; print(rH(a))); print(v); print(rH(a));", this.SRC_FOLDER_PATH + "\\log13.in");
+	}
+	
 	public MyList<Example> getAllExamples() {
 		MyList<Example> exampleList = new MyList<Example>();
 		
@@ -298,6 +331,7 @@ public class AllExamples {
 		exampleList.addLast(this.getExample10());
 		exampleList.addLast(this.getExample11());
 		exampleList.addLast(this.getExample12());
+		exampleList.addLast(this.getExample13());
 
 		return exampleList;
 	}
