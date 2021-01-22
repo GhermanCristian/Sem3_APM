6556f50463dd91ebcd07a896500fe5b862612732
GhermanCristian <gherman_cristi10@yahoo.com>
2021-01-22 14:07:53 +0200

    - Added CreateBarrierStatement

---
+++ PuyaInterpreter/src/model/statement/CreateBarrierStatement.java
@@ -0,0 +1,64 @@
+package model.statement;
+
+import java.util.ArrayList;
+import exception.InvalidTypeException;
+import exception.UndefinedVariableException;
+import javafx.util.Pair;
+import model.ProgramState;
+import model.ADT.DictionaryInterface;
+import model.ADT.MyLockTable;
+import model.expression.ExpressionInterface;
+import model.type.IntType;
+import model.type.TypeInterface;
+import model.value.IntValue;
+import model.value.ValueInterface;
+
+public class CreateBarrierStatement implements StatementInterface {
+	private final String indexVariableName;
+	private final ExpressionInterface totalPermitCountExpression;
+
+	public CreateBarrierStatement(String indexVariableName, ExpressionInterface totalPermitCountExpression) {
+		this.indexVariableName = indexVariableName;
+		this.totalPermitCountExpression = totalPermitCountExpression;
+	}
+	
+	@Override
+	public ProgramState execute(ProgramState crtState) throws Exception {
+		DictionaryInterface<String, ValueInterface> symbolTable = crtState.getSymbolTable();
+		DictionaryInterface<Integer, ValueInterface> heap = crtState.getHeap();
+		DictionaryInterface<Integer, Pair<Integer, ArrayList<Integer>>> barrierTable = crtState.getBarrierTable();
+		
+		if (symbolTable.isDefined(this.indexVariableName) == false) {
+			throw new UndefinedVariableException("CreateBarrierStatement: Variable " + this.indexVariableName + " is not defined in the symbolTable");
+		}
+		
+		ValueInterface capacity = this.totalPermitCountExpression.evaluate(symbolTable, heap);
+		int newPositionInBarrierTable = ((MyLockTable<Integer, Pair<Integer, ArrayList<Integer>>>)(barrierTable)).getFirstAvailablePosition();
+		barrierTable.insert(newPositionInBarrierTable, new Pair<Integer, ArrayList<Integer>>(((IntValue)capacity).getValue(), new ArrayList<Integer>()));
+		symbolTable.update(this.indexVariableName, new IntValue(newPositionInBarrierTable));
+		
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		String representation = "";
+		representation += ("createBarrier(" + this.indexVariableName + ", " + this.totalPermitCountExpression.toString() + ");\n");
+		return representation;
+	}
+
+	@Override
+	public DictionaryInterface<String, TypeInterface> getTypeEnvironment(
+			DictionaryInterface<String, TypeInterface> initialTypeEnvironment) throws Exception {
+		if (initialTypeEnvironment.isDefined(this.indexVariableName) == false) {
+			throw new UndefinedVariableException("CreateBarrierStatement: Variable " + this.indexVariableName + " is not defined in the typeEnvironment");
+		}
+		if (initialTypeEnvironment.getValue(this.indexVariableName).equals(new IntType()) == false) {
+			throw new InvalidTypeException("CreateBarrierStatement: Variable " + this.indexVariableName + " is not an integer");
+		}
+		if (this.totalPermitCountExpression.typeCheck(initialTypeEnvironment).equals(new IntType()) == false) {
+			throw new InvalidTypeException("CreateBarrierStatement: Capacity expression is not integer");
+		}
+		return initialTypeEnvironment;
+	}
+}
